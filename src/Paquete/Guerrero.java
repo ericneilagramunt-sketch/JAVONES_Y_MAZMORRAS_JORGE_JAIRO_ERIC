beb9a150c6061d8f1aa96053be362bc31fa9c908
ericneilagramunt-sketch <ericneilagramunt@alumnes.ilerna.com>
2026-02-09 17:18:17 +0100

    Creo la funcion de proteger

--- src/Paquete/Guerrero.java
+++ src/Paquete/Guerrero.java
@@ -2,5 +2,14 @@ package Paquete;
 
 public class Guerrero {
 
+ private Personaje CompañeroProtegido;
  
+ 
+ public Guerrero() {
+	 this.CompañeroProtegido = null;
+ }
+ 
+ public void proteger(Personaje compañero) {
+	 this.CompañeroProtegido = compañero;
+ }
 }
