3b5d71afb9e52d8d390db335f2fdb601efb57253
ericneilagramunt-sketch <ericneilagramunt@alumnes.ilerna.com>
2026-02-09 16:34:14 +0100

    Creo el metodo estainvisible

--- src/Paquete/Ladron.java
+++ src/Paquete/Ladron.java
@@ -4,9 +4,27 @@ public class Ladron {
 	
 	private boolean invisible;
 	
-	public void ladron();
+	public void ladron() {
 	
 	this.invisible = false;
 
 
+}
+
+public void hacerseinvisible() {
+	
+	this.invisible = !this.invisible;
+	
+	if (this.invisible){
+		System.out.println("El ladron es invisible");
+	}
+	
+	else {
+		System.out.println("El ladron ya no es invisible");
+	}
+	}
+
+  public boolean estainvisible() {
+	return this.invisible;
+}
 }
