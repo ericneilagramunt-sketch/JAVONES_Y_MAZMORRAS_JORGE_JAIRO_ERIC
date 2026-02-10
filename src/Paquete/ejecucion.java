4351b59d2cfb6d2ed31986d9343dd088fcf63250
jorgeclarosala-hash <jorgeclarosala@alumnes.ilerna.com>
2026-02-10 20:27:42 +0100

    Creamos la introduccion, el titulo del juego

---
+++ src/Paquete/ejecucion.java
@@ -0,0 +1,11 @@
+
+public class ejecucion {
+	public static void main(String[] args) {
+		
+	System.out.println("====================================");
+	    System.out.println("BIENVENIDO A JAVONES Y MAZMORRAS");
+	    System.out.println("====================================\n");
+	    
+	    
+}
+}
