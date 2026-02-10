52a93272c4fb2687fdaf32ae37ef7a94a0cb4594
jorgeclarosala-hash <jorgeclarosala@alumnes.ilerna.com>
2026-02-10 20:33:59 +0100

    Creacion de los personajes

--- src/Paquete/ejecucion.java
+++ src/Paquete/ejecucion.java
@@ -6,6 +6,19 @@ public class ejecucion {
 	    System.out.println("BIENVENIDO A JAVONES Y MAZMORRAS");
 	    System.out.println("====================================\n");
 	    
+	    // ===== CREANDO PERSONAJES =====
+	    System.out.println("--- Creando personajes ---\n");
+	    
+	    // Crear un Mago
+	    Mago mago = new Mago("Gandalf", 5, 100, 50);
+	    
+	    // Crear un Guerrero
+	    Guerrero guerrero = new Guerrero("Conan", 6, 150);
+	    
+	    // Crear un Ladrón
+	    Ladron ladron = new Ladron("Robin", 4, 80);
+	    
+	    System.out.println("¡Personajes creados!\n");
 	    
 }
 }
