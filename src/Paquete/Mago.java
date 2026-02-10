113a82a0bacf5b8afdd7eda038b2773e394f52ba
jorgeclarosala-hash <jorgeclarosala@alumnes.ilerna.com>
2026-02-10 13:58:41 +0100

    Un constructor importante
    En la clase mago creamos el constructor que que hereda atributos de la clase Personaje ademas de llamarlos con el "super" tambien tenemos un nuevo objeto unico del mago que es el "mana"

--- src/Paquete/Mago.java
+++ src/Paquete/Mago.java
@@ -2,4 +2,13 @@ package Paquete;
 
 public class Mago {
 
+public class Mago extends Personaje{
+    //objeto unico del mago
+    private int mana;
+ // Constructor - Crea un nuevo Mago
+    // "super" llama al constructor de la clase padre (Personaje)
+    public Mago(String nombre, int nivel, int puntosVida, int mana) {
+        super(nombre, nivel, puntosVida);  // Llama al constructor de Personaje
+        this.mana = mana;
+    }
 }
